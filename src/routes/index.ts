514c57aa17536b30fba364a3fbb417157158c41c
eduardscaueru <44285293+eduardscaueru@users.noreply.github.com>
2023-04-29 13:43:15 +0300

    Removed comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,8 @@
 import {Router} from "express";
 import auth from "./auth.routes";
-// import user from "./user.routes";
-// import feed from "./feed.router";
-// import group from "./group.routes";
-// import post from "./post.routes";
-// import chat from "./chat.routes";
-// import comment from "./comment.routes"
 
 const routes = Router();
 
 routes.use("/auth", auth);
-// routes.use("/user", user);
-// routes.use("/feed", feed);
-// routes.use("/group", group);
-// routes.use("/post", post);
-// routes.use("/chat", chat);
-// routes.use("/comment", comment);
 
 export default routes;
